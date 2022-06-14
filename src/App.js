b6527491f92d04cb8089b78448496a0aa9689c21
James <jamesthao32@gmail.com>
2022-06-13 19:07:05 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
-
-import Clock from "./Clock";
-
+import React, { useEffect, useState } from "react";
+import Clock  from "./Clock";
 function App() {
   const [showClock, setShowClock] = useState(true);
+  const [time ,setTime]=useState(new Date())
+useEffect(()=>{
+const timer=setInterval(() => {
+  setTime(new Date())
+}, 1000);
+return ()=>{
+  clearInterval(timer)
+}
 
-  return (
-    <div>
-      {showClock ? <Clock /> : null}
-      <button onClick={() => setShowClock(!showClock)}>Toggle Clock</button>
-    </div>
-  );
+},[])
+
+return (
+  <div>
+    {showClock ? <Clock /> : null}
+    <button onClick={() => setShowClock(false)}>Toggle Clock</button>
+{time.toString()}
+  </div>
+);
 }
 
 export default App;
+
